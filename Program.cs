bb16896d4675c8f449ccefeb6a02797e3d8a8dec
Dark045NL <87452921+Dark045NL@users.noreply.github.com>
2021-12-17 13:04:27 +0100

    Calc v2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//user inputs 5 numbers
+//user gets 2 highest numbers in return
+
+//Declaring variables
+int iMax = 0;
+int iSecMax = 0;
+int iInput = 0;
+string sInput;
+
+for (int i = 0; i < 5; i++)
+{
+    sInput = Console.ReadLine();
+    if(!int.TryParse(sInput, out iInput)){
+        Console.WriteLine("Dit is geen getal");
+        i--;
+    }
+    else{
+        iInput = int.Parse(sInput);
+        if (iInput < iMax){
+            iSecMax = iMax;
+            iMax = iInput;
+        }
+    }
+}
+Console.WriteLine(iSecMax);
+Console.WriteLine(iMax);
